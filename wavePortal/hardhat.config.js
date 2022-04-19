9749c89118e4e9a40da591e297932f9f8a546d0d
heeze <96997740+rolic-hub@users.noreply.github.com>
2022-04-19 11:36:45 +0100

    Update hardhat.config.js

--- wavePortal/hardhat.config.js
+++ wavePortal/hardhat.config.js
@@ -1,12 +1,8 @@
-//5K83KJ8roLt-gHT6K64ovhL6C4UE5_7s
-// 0xd79b1585531e57A8d239ADc5262398279b4F5c91
+
 
 require("@nomiclabs/hardhat-waffle");
-
-
-// module.exports = {
-//   solidity: "0.8.4",
-// };
+import dotenv from "dotenv";
+dotenv.config();
 
 module.exports = {
   solidity: "0.8.0",
